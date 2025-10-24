9a661baed56b075c6a484ec0a4f298ae3086b12e
Avi <95044844+Avioti@users.noreply.github.com>
2025-10-24 11:46:08 -0400

    Phase 1

--- src/main/java/com/pluralsight/Dealership.java
+++ src/main/java/com/pluralsight/Dealership.java
@@ -1,4 +1,82 @@
 package com.pluralsight;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class Dealership {
+    private String name,address,phone;
+    private ArrayList<Vehicle> inventory;
+
+    public Dealership(String name, String address, String phone) {
+        this.name = name;
+        this.address = address;
+        this.phone = phone;
+        inventory = new ArrayList<>();
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getAddress() {
+        return address;
+    }
+
+    public void setAddress(String address) {
+        this.address = address;
+    }
+
+    public String getPhone() {
+        return phone;
+    }
+
+    public void setPhone(String phone) {
+        this.phone = phone;
+    }
+
+    public ArrayList<Vehicle> getVehiclesByPrice(double min, double max){
+        return null;
+    }
+
+    public ArrayList<Vehicle> getVehiclesByMakeModel(String make, String model){
+        return null;
+    }
+
+    public ArrayList<Vehicle> getVehiclesByYear(int min, int max){
+        return null;
+    }
+
+    public ArrayList<Vehicle> getVehiclesByColor(String color){
+        return null;
+    }
+
+    public ArrayList<Vehicle> getVehiclesByMileage(int min, int max){
+        return null;
+    }
+
+    public ArrayList<Vehicle> getVehiclesByType(String vehicleType){
+        return null;
+    }
+
+    public List<Vehicle> getAllVehicle(){
+
+        return inventory;
+    }
+
+
+
+    public boolean addVehicle(Vehicle vehicle){
+
+        return inventory.add(vehicle);
+
+    }
+
+    public void removeVehicle(Vehicle vehicle){
+
+    }
+
 }
--- src/main/java/com/pluralsight/Program.java
+++ src/main/java/com/pluralsight/Program.java
@@ -1,7 +1,12 @@
 package com.pluralsight;
 
+import java.util.Scanner;
+
 public class Program {
+    public static Scanner scanner;
     public static void main(String[] args) {
+        scanner = new Scanner(System.in);
+
 
     }
 }
