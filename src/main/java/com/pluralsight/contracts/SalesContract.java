9db5233c35d8c97501dd687120c96c662ef8c959
Avi <95044844+Avioti@users.noreply.github.com>
2025-10-30 17:21:19 -0400

    salesContract done

--- src/main/java/com/pluralsight/contracts/SalesContract.java
+++ src/main/java/com/pluralsight/contracts/SalesContract.java
@@ -2,13 +2,13 @@ package com.pluralsight.contracts;
 
 import com.pluralsight.models.Vehicle;
 
-public class SalesContract extends Contract{
-    private double salesTax,processingFee;
+public class SalesContract extends Contract {
+    private double salesTax, processingFee;
     private double recordingFee = 100;
     private boolean financeOption;
 
 
-    public SalesContract(String date, String customerName, String customerEmail, Vehicle vehicle, double salesTax, double processingFee, boolean financeOption) {
+    public SalesContract(String date, String customerName, String customerEmail, Vehicle vehicle, boolean financeOption) {
         super(date, customerName, customerEmail, vehicle);
         this.salesTax = 0.05;
         this.processingFee = 295;
@@ -30,11 +30,39 @@ public class SalesContract extends Contract{
 
     @Override
     public double getTotalPrice() {
-        return 0;
+
+        double salesPrice = vehicle.getPrice() * salesTax;
+
+        if (vehicle.getPrice() >= 10000) {
+            processingFee = 495;
+        }
+        totalPrice = vehicle.getPrice() + salesPrice + recordingFee + processingFee;
+        return totalPrice;
     }
 
     @Override
     public double getMonthlyPayment() {
-        return 0;
+        if (!financeOption) {
+            return 0;
+        }
+        double monthlyRate = 0;
+
+        int months = 0;
+        totalPrice = getTotalPrice();
+        if (vehicle.getPrice() > 10000) {
+            salesTax = 0.0425;
+            months = 48;
+        } else {
+            salesTax = 0.0525;
+            months = 24;
+        }
+
+        monthlyRate = salesTax / 12;
+
+        monthlyPayment = totalPrice * (monthlyRate * Math.pow(1 + monthlyRate, months))
+                / (Math.pow(1 + monthlyRate, months) - 1);
+
+
+        return monthlyPayment;
     }
 }
